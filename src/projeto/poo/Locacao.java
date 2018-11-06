3f8db67a3fb1b5db91b2cc4e2e8e1672de0dc001
lukasbp <44759049+lukasbp@users.noreply.github.com>
2018-11-06 20:51:40 -0200

    Add files via upload

--- src/projeto/poo/Locacao.java
+++ src/projeto/poo/Locacao.java
@@ -1,79 +1,99 @@
+package aula;
+
 import java.util.Calendar;
 import java.util.Scanner;
 
 public class Locacao {
-	private Calendar dt_locacao, dt_devolucao,dt_DevolucaoPrevista;
-	private long km_locacao,km_devolucao;
-	private double valor_caucao, valor_locacao;
-	private int locacao_ID;
+	private Cliente cliente;
+	private Calendar dtLocacao, dtDevolucao, dtDevolucaoPrevista;
+	private long kmLocacao, kmDevolucao;
+	private double valorCaucao, valorLocacao;
+	private int locacaoID;
 	private boolean paga;
 
-	public Locacao(Calendar dt_locacao, Calendar dt_devolucao, Calendar dt_DevolucaoPrevista, long km_locacao, long km_devolucao,
-			double valor_caucao, double valor_locacao,boolean paga) {
-		this.dt_locacao = dt_locacao;
-		this.dt_devolucao = dt_devolucao;
-		this.dt_DevolucaoPrevista = dt_DevolucaoPrevista;
-		this.km_locacao = km_locacao;
-		this.km_devolucao = km_devolucao;
-		this.valor_caucao = valor_caucao;
-		this.valor_locacao = valor_locacao;
+	public Locacao(Cliente cliente, Calendar dtLocacao, Calendar dtDevolucao, Calendar dtDevolucaoPrevista,
+			long kmLocacao, long kmDevolucao, double valorCaucao, double valorLocacao, boolean paga) {
+		this.cliente = cliente;
+		this.dtLocacao = dtLocacao;
+		this.dtDevolucao = dtDevolucao;
+		this.dtDevolucaoPrevista = dtDevolucaoPrevista;
+		this.kmLocacao = kmLocacao;
+		this.kmDevolucao = kmDevolucao;
+		this.valorCaucao = valorCaucao;
+		this.valorLocacao = valorLocacao;
 		this.paga = paga;
 	}
 
-	public Calendar getDt_locacao() {
-		return dt_locacao;
+	public Cliente getCliente() {
+		return cliente;
 	}
 
-	public void setDt_locacao(Calendar dt_locacao) {
-		this.dt_locacao = dt_locacao;
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 
-	public Calendar getDt_devolucao() {
-		return dt_devolucao;
+	public Calendar getDtLocacao() {
+		return dtLocacao;
 	}
 
-	public void setDt_devolucao(Calendar dt_devolucao) {
-		this.dt_devolucao = dt_devolucao;
+	public void setDtLocacao(Calendar dtLocacao) {
+		this.dtLocacao = dtLocacao;
 	}
 
-	public Calendar getDt_DevolucaoPrevista() {
-		return dt_DevolucaoPrevista;
+	public Calendar getDtDevolucao() {
+		return dtDevolucao;
 	}
 
-	public void setDt_DevolucaoPrevista(Calendar dt_DevolucaoPrevista) {
-		this.dt_DevolucaoPrevista = dt_DevolucaoPrevista;
+	public void setDtDevolucao(Calendar dtDevolucao) {
+		this.dtDevolucao = dtDevolucao;
 	}
 
-	public long getKm_locacao() {
-		return km_locacao;
+	public Calendar getDtDevolucaoPrevista() {
+		return dtDevolucaoPrevista;
 	}
 
-	public void setKm_locacao(long km_locacao) {
-		this.km_locacao = km_locacao;
+	public void setDtDevolucaoPrevista(Calendar dtDevolucaoPrevista) {
+		this.dtDevolucaoPrevista = dtDevolucaoPrevista;
 	}
 
-	public long getKm_devolucao() {
-		return km_devolucao;
+	public long getKmLocacao() {
+		return kmLocacao;
 	}
 
-	public void setKm_devolucao(long km_devolucao) {
-		this.km_devolucao = km_devolucao;
+	public void setKmLocacao(long kmLocacao) {
+		this.kmLocacao = kmLocacao;
 	}
 
-	public double getValor_caucao() {
-		return valor_caucao;
+	public long getKmDevolucao() {
+		return kmDevolucao;
 	}
 
-	public void setValor_caucao(double valor_caucao) {
-		this.valor_caucao = valor_caucao;
+	public void setKmDevolucao(long kmDevolucao) {
+		this.kmDevolucao = kmDevolucao;
 	}
 
-	public int getLocacao_ID() {
-		return locacao_ID;
+	public double getValorCaucao() {
+		return valorCaucao;
 	}
 
-	public void setLocacao_ID(int locacao_ID) {
-		this.locacao_ID = locacao_ID;
+	public void setValorCaucao(double valorCaucao) {
+		this.valorCaucao = valorCaucao;
+	}
+
+	public double getValorLocacao() {
+		return valorLocacao;
+	}
+
+	public void setValorLocacao(double valorLocacao) {
+		this.valorLocacao = valorLocacao;
+	}
+
+	public int getLocacaoID() {
+		return locacaoID;
+	}
+
+	public void setLocacaoID(int locacaoID) {
+		this.locacaoID = locacaoID;
 	}
 
 	public boolean isPaga() {
@@ -84,44 +104,52 @@ public class Locacao {
 		this.paga = paga;
 	}
 
-	public double getValor_locacao() {
-		return valor_locacao;
-	}
-
-	public void setValor_locacao(double valor_locacao) {
-		this.valor_locacao = valor_locacao;
-	}
-	
 	public static Locacao criar() {
-		Scanner sc = new Scanner (System.in);
-		Calendar dt_locacao, dt_devolucao, dt_DevolucaoPrevista;
-		long km_locacao, km_devolucao;
-		double valor_caucao, valor_locacao;
+		Scanner sc = new Scanner(System.in);
+		Cliente cliente;
+		int cli;
+		Calendar dtLocacao, dtDevolucao, dtDevolucaoPrevista;
+		long kmLocacao, kmDevolucao;
+		double valorCaucao, valorLocacao;
 		boolean paga;
-		System.out.println("data de locacao do veiculo");
+		System.out.println("Cliente pf ou pj, digite 1 para pf e 2 para pj.");
+		cli = Leitura.lerInt(); 
+		if(cli == 1) {
+			cliente = ClientePF.criar();
+		}
+		else if(cli == 2) {
+			cliente = ClientePJ.criar();
+		}
+		else {
+			System.out.println("digite um dos dois numeros propostos.");//Provisório
+			return criar();
+		}
+		System.out.println("data de locacao do veiculo");//atributos calendar para implementar
 		System.out.println("data de devolucao:");
 		System.out.println("data prevista para devolucao:");
 		System.out.println("insira o km de locacao do veiculo:");
-		km_locacao = sc.nextLong();
+		kmLocacao = (long) Leitura.lerInt();
 		System.out.println("insira a quilometragem que foi devolvido o veiculo");
-		km_devolucao = sc.nextLong();
+		kmDevolucao = (long) Leitura.LerInt();
 		System.out.println("insira o valor de caucao:");
-		valor_caucao = sc.nextDouble();
+		valorCaucao = Leitura.lerDouble();
 		System.out.println("insira o valor de locacao do veiculo:");
-		valor_locacao = sc.nextDouble();
-		System.out.println("a locacao foi paga?");
-		paga = sc.nextBoolean();
-		Locacao lc = new Locacao(dt_locacao,dt_devolucao,dt_DevolucaoPrevista,km_locacao,km_devolucao,valor_caucao,valor_locacao,paga);
+		valorLocacao = Leitura.lerDouble();
+		System.out.println("a locacao foi paga? digite 1 para sim, e 2 para nao.");
+		paga = Boolean.getBoolean (Leitura.lerString());
+		Locacao lc = new Locacao(cliente, dtLocacao, dtDevolucao, dtDevolucaoPrevista, kmLocacao, kmDevolucao, valorCaucao,
+				valorLocacao, paga);
 		return lc;
 	}
 
 	@Override
 	public String toString() {
-		return "Locacao [dt_locacao=" + dt_locacao + ", dt_devolucao=" + dt_devolucao + ", dt_DevolucaoPrevista="
-				+ dt_DevolucaoPrevista + ", km_locacao=" + km_locacao + ", km_devolucao=" + km_devolucao
-				+ ", valor_caucao=" + valor_caucao + ", valor_locacao=" + valor_locacao + ", locacao_ID=" + locacao_ID
-				+ ", paga=" + paga + "]";
+		return "Locacao [dtLocacao=" + dtLocacao + ", dtDevolucao=" + dtDevolucao + ", dtDevolucaoPrevista="
+				+ dtDevolucaoPrevista + ", kmLocacao=" + kmLocacao + ", kmDevolucao=" + kmDevolucao + ", valorCaucao="
+				+ valorCaucao + ", valorLocacao=" + valorLocacao + ", locacaoID=" + locacaoID + ", paga=" + paga + "]";
 	}
 
 	
+	
+
 }

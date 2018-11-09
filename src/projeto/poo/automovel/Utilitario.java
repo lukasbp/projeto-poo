d85ec6544c9d6c66736d2141ad2be8e599755634
lukasbp <44759049+lukasbp@users.noreply.github.com>
2018-11-08 22:34:19 -0200

    Update Utilitario.java

--- src/projeto/poo/automovel/Utilitario.java
+++ src/projeto/poo/automovel/Utilitario.java
@@ -2,50 +2,39 @@ package projeto.poo.automovel;
 
 import projeto.poo.Leitura;
 
-public class Utilitario extends Automovel {
+public class Utilitario extends Veiculo {
 
 	private int capacidadeCarga, nroEixos;
-	private boolean escolta;
 
-	public Utilitario(int automovelID, String placa, String cor, int nroPortas, char tipoCombustivel,
-			long quilometragem, double custo, int capacidadeCarga, int nroEixos, boolean escolta) {
-		super(automovelID, placa, cor, nroPortas, tipoCombustivel, quilometragem, custo);
+	public Utilitario(int veiculoID, String placa, String cor, int nroPortas, char tipoCombustivel,
+			long quilometragem, double valorDiaria, int capacidadeCarga, int nroEixos) {
+		super(veiculoD, placa, cor, nroPortas, tipoCombustivel, quilometragem, valorDiaria);
 		this.capacidadeCarga = capacidadeCarga;
 		this.nroEixos = nroEixos;
-		this.escolta = escolta;
 	}
 
-	public Utilitario(Automovel a, int capacidadeCarga, int nroEixos, boolean escolta) {
-		super(a.getAutomovelID(), a.getPlaca(), a.getCor(), a.getNroPortas(), a.getTipoCombustivel(),
-				a.getQuilometragem(), a.getCusto());
+	public Utilitario(Veiculo a, int capacidadeCarga, int nroEixos) {
+		super(a.getVeiculoID(), a.getPlaca(), a.getCor(), a.getNroPortas(), a.getTipoCombustivel(),
+				a.getQuilometragem(), a.getValorDiaria());
 		this.capacidadeCarga = capacidadeCarga;
 		this.nroEixos = nroEixos;
-		this.escolta = escolta;
 	}
 
 	public static Utilitario criar() {
 		Automovel a = Automovel.criar();
 		int capacidadeCarga, nroEixos;
-		boolean escolta;
 		System.out.println("Digite a capacidade de carga:");
 		capacidadeCarga = Leitura.lerInt();
 		System.out.println("Digite o número de eixos:");
 		nroEixos = Leitura.lerInt();
-		System.out.println("O veículo possui escolta? (1 - sim | 2 - não)");
-		escolta = Boolean.getBoolean(Leitura.lerString());
-		return new Utilitario(a, capacidadeCarga, nroEixos, escolta);
+		return new Utilitario(a, capacidadeCarga, nroEixos);
 	}
 
 	@Override
 	public String toString() {
-		return "Executivo [\n\tcapacidadeCarga=" + capacidadeCarga + "\n\tnroEixos=" + nroEixos + "\n\tescolta="
-				+ escolta + super.toString() + "]";
+		return "Executivo [\n\tcapacidadeCarga=" + capacidadeCarga + "\n\tnroEixos=" + nroEixos + super.toString() + "]";
 	}
-
-	public double calcularCustos() {
-		return super.getCusto(); // Provisório
-	}
-
+//retirei o metodo calcular gastos pois esta sendo herdado pelo veiculo
 	public int getCapacidadeCarga() {
 		return capacidadeCarga;
 	}
@@ -60,14 +49,6 @@ public class Utilitario extends Automovel {
 
 	public void setNroEixos(int nroEixos) {
 		this.nroEixos = nroEixos;
-	}
-
-	public boolean isEscolta() {
-		return escolta;
-	}
-
-	public void setEscolta(boolean escolta) {
-		this.escolta = escolta;
 	}
 
 }
